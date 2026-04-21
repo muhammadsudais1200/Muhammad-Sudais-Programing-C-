388b95f60bd3110ac389b70978ed80b89d3edf38
Muhammad Sudais <m.s.u.d.a.i.s7772@gmail.com>
2026-04-21 15:25:52 +0500

    Add files via upload

---
+++ Lab 6/Lab Work/LAB_6 LabWork_1.cpp
@@ -0,0 +1,18 @@
+// Name: Muhammad Sudais
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0757
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;   // starting point
+
+    while (i <= 10) {
+        cout << i << endl;  // print number on new line
+        i++;                // move to next number
+    }
+
+    return 0;
+}
